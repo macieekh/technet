45a7f0dfc0591c9b5dc1f4379a6471d865271b10
Maciej Harazinski <maciej.harazinski@gmail.com>
2014-07-03 00:04:40 +0200

    working multiple order_ticket_line YEAH!

--- app/models/order_ticket.rb
+++ app/models/order_ticket.rb
@@ -1,5 +1,5 @@
 class OrderTicket < ActiveRecord::Base
 	# has_and_belongs_to_many :parts
-	has_many :lines
-	has_many :parts, :through => :lines
+	has_many :order_ticket_lines
+	has_many :parts, :through => :order_ticket_lines
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,7 +1,7 @@
 class Part < ActiveRecord::Base
 	has_and_belongs_to_many :models
 	# has_and_belongs_to_many :order_tickets
-	has_many :lines
-	has_many :order_tickets, :through => :lines
+	has_many :order_ticket_lines
+	has_many :order_tickets, :through => :order_ticket_lines
 	accepts_nested_attributes_for :models
 end
