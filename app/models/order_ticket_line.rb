19fed22f47f1c2b4b9b47d87ba06c160489bec70
Maciej Harazinski <maciej.harazinski@gmail.com>
2014-07-04 22:55:09 +0200

    comments

--- app/models/order_ticket_line.rb
+++ app/models/order_ticket_line.rb
@@ -1,3 +1,4 @@
+# This model defines single line in the OrderTicket. There might be many OrderTicketLines per OrderTicket
 class OrderTicketLine < ActiveRecord::Base
 	belongs_to :order_ticket
 	belongs_to :part
